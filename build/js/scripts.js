6bf260b26d0dd64c30eb1420e563ed0535918c42
RuslanZinchenko <zinchenko.rusya@gmail.com>
2019-01-18 15:25:16 +0200

    rb

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,7 +1,15 @@
 "use strict";
 
+var remove = document.querySelector('.remove');
+var removeImg = document.querySelector('img');
 document.addEventListener('DOMContentLoaded', getFromLS);
+remove.addEventListener('click', removeLS);
+removeImg.addEventListener('click', removeLS);
 
 function getFromLS() {
+  localStorage.clear();
+}
+
+function removeLS() {
   localStorage.clear();
 }
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,15 @@
+
+const remove = document.querySelector('.remove');
+const removeImg = document.querySelector('img');
+
 document.addEventListener('DOMContentLoaded', getFromLS);
+remove.addEventListener('click', removeLS);
+removeImg.addEventListener('click', removeLS);
+
 function getFromLS(){
+    localStorage.clear();
+}
+
+function removeLS(){
     localStorage.clear();
 }
