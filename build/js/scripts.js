978adde042d9a3048da4429ec3aace7dc0b912fc
RuslanZinchenko <zinchenko.rusya@gmail.com>
2019-01-18 16:06:47 +0200

    lr

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,15 +1,27 @@
 "use strict";
 
+var add = document.querySelector('.add');
 var remove = document.querySelector('.remove');
-var removeImg = document.querySelector('img');
+var foto = document.querySelector('img');
+var global;
 document.addEventListener('DOMContentLoaded', getFromLS);
+add.addEventListener('click', createLS);
 remove.addEventListener('click', removeLS);
-removeImg.addEventListener('click', removeLS);
+
+function createLS() {
+  var fotka = 'https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  localStorage.setItem('first', fotka);
+  location.reload();
+}
+
+;
 
 function getFromLS() {
-  localStorage.clear();
+  var getLS = localStorage.getItem('first');
+  foto.setAttribute('src', getLS);
 }
 
 function removeLS() {
   localStorage.clear();
+  location.reload();
 }
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,26 @@
 
+const add = document.querySelector('.add');
 const remove = document.querySelector('.remove');
-const removeImg = document.querySelector('img');
+const foto = document.querySelector('img');
+let global;
 
 document.addEventListener('DOMContentLoaded', getFromLS);
+add.addEventListener('click', createLS);
 remove.addEventListener('click', removeLS);
-removeImg.addEventListener('click', removeLS);
+
+function createLS(){
+    const fotka = 'https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+    localStorage.setItem('first', fotka);
+    location.reload();
+};
 
 function getFromLS(){
-    localStorage.clear();
+    const getLS = localStorage.getItem('first');
+    foto.setAttribute('src', getLS);
 }
 
 function removeLS(){
     localStorage.clear();
+    location.reload();
 }
+
